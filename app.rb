125afba9054bdd96972722b8c50ec971deb43181
Reiyaman <Reiyaman>
2021-09-08 18:40:37 +0000

    ログイン画面と新規登録ボタン実装

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ require 'sinatra/reloader' if development?
 require './models'
 require 'dotenv/load'
 
+enable :sessions
+
 get '/' do
     erb :index
+end
+
+get '/sign_up' do
+    erb :sign_up
+end
+
+post 'signup' do
+    
+    redirect '/'
 end
