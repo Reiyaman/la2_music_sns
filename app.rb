69b3252bb19170b8a493ae1405e3c64fea64c855
Reiyaman <Reiyaman>
2021-09-09 05:06:05 +0000

    新規登録機能実装

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ require 'sinatra/reloader' if development?
 require './models'
 require 'dotenv/load'
 
-enable :sessions
+enable :sessions #セッション機能
+
+helpers do #ログイン中のユーザー情報取得
+    def current_user
+        User.find_by(id: session[:user])
+    end
+end
 
 get '/' do
     erb :index
@@ -14,7 +20,39 @@ get '/sign_up' do
     erb :sign_up
 end
 
-post 'signup' do
-    
+post '/sign_up' do
+    user = User.create(
+        name: params[:name],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation],
+        profile: params[:profile])
+    if user.persisted? #データベースに保存されたかどうか
+        session[:user] = user.id
+        redirect 'search'
+    else
+        redirect '/'
+    end
+end
+
+post '/sign_in' do
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+        redirect '/search'
+    else
+        redirect '/'
+    end
+end
+
+get '/sign_out' do
+    session[:user] = nil
     redirect '/'
+end
+
+get '/search' do
+    erb :search
+end
+
+get '/home' do
+    erb :home
 end
