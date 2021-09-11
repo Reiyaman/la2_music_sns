b8f2328ecf3eb7e786db4b8b7d8de181b372ef50
Reiyaman <Reiyaman>
2021-09-11 10:14:33 +0000

    プロフィール画像の表示

--- app.rb
+++ app.rb
@@ -12,6 +12,15 @@ helpers do #ログイン中のユーザー情報取得
     end
 end
 
+before do
+    Dotenv.load
+    Cloudinary.config do |config|
+        config.cloud_name = ENV['CLOUD_NAME']
+        config.api_key = ENV['CLOUDINARY_API_KEY']
+        config.api_secret = ENV['CLOUDINARY_API_SECRET']
+    end
+end
+
 get '/' do
     erb :index
 end
@@ -21,11 +30,21 @@ get '/sign_up' do
 end
 
 post '/sign_up' do
-    user = User.create(
+    img_url = ''
+    if params[:profile]
+        img = params[:profile]
+        tempfile = img[:tempfile]
+        upload = Cloudinary::Uploader.upload(tempfile.path)
+        img_url = upload['url']
+    end
+    
+    user = User.create({
         name: params[:name],
         password: params[:password],
         password_confirmation: params[:password_confirmation],
-        profile: params[:profile])
+        profile: img_url
+    })
+    
     if user.persisted? #データベースに保存されたかどうか
         session[:user] = user.id
         redirect '/search'
@@ -60,7 +79,7 @@ end
 post '/delete/:id' do
     song = Song.find(params[:id])
     song.destroy
-    redirect 'home'
+    redirect '/home'
 end
 
 get '/edit/:id' do
